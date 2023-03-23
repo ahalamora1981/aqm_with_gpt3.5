83beb22f540a39803354b9e20c2819ef4c58b81f
ahalamora1981 <96462464+ahalamora1981@users.noreply.github.com>
2023-03-23 16:20:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,26 +2,34 @@ import streamlit as st
 import openai
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-def check_bad_language(text):
-    prompt_for_bad_words = "你是一个客户服务质检员，判断输入的文字是否有不良用语，有就回答“是”，没有就回答“否”，不要给出任何多余的答复和标点。"
-
+def check(text_input, prompt):
     response = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo",
-      messages=[
-            {"role": "system", "content": prompt_for_bad_words},
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": prompt},
             {"role": "user", "content": text}
         ]
     )
-    
     return response['choices'][0]['message']['content']
 
 st.title("AQM with GPT3.5")
-st.subheader("判断是否有不良用语")
 
-text_input = st.text_input("请输入文本")
+option = st.selectbox(
+    "自动质检点类型:",
+    ("不良用语", "负面情绪", "投诉")
+)
 
-if st.button("提交"):
-    response = check_bad_language(text_input)
+text_input = st.text_input()
+
+if st.button("提交", use_container_width=True):
+    if option = "不良用语":
+        prompt = "你是一个客户服务质检员，判断输入的文字是否有不良用语，有就回答“是”，没有就回答“否”，不要给出任何多余的答复和标点。"
+    elif option = "":
+        prompt = "你是一个客户服务质检员，判断输入的文字是否有负面情绪，有就回答“是”，没有就回答“否”，不要给出任何多余的答复和标点。"
+    elif option = "":
+        prompt = "你是一个客户服务质检员，判断输入的文字是否提到投诉，必须是明确提到投诉才算，有就回答“是”，没有就回答“否”，不要给出任何多余的答复和标点。"
+        
+    response = check(text_input, prompt)
 
     if "是" in response:
         st.write("有不良用语")
